58464131d673f744699b581cf251eac630ead341
ilKin0609 <ilkinmammadov.11@icloud.com>
2025-11-25 21:36:34 +0400

    Task2 completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
 // Task 1
-function CheckDivisibility(num) {
-    if (num % 3 === 0 && num % 7 === 0)
-        return true;
-    return false;
-}
+//function CheckDivisibility(num) {
+    //if (num % 3 === 0 && num % 7 === 0)
+       // return true;
+    //return false;
+///}
 
-console.log(CheckDivisibility(20));
+//console.log(CheckDivisibility(20));
+
+// Task 2
+function Factorial(n) {
+    let factorial=1;
+    if (n === 0 || n === 1)
+        return 1;
+    else if (n < 0)
+        return -1;
+    for (let i = 2; i <= n; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+console.log(Factorial(5));
+
+// Task 3
 
         
